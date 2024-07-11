f55e88fda87ef9dd35b3d16024b4209145389b57
mj0820 <mujun0820@gmail.com>
2024-07-11 13:59:01 +0900

    edit login

--- loginfuntion.js
+++ loginfuntion.js
@@ -2,5 +2,5 @@ let login_button = document.querySelector('#login_button');
 
 login_button.addEventListener('click', function (){
     console.log("ok");
-    window.location.href = 'https://auth.dimigo.net/oauth?client=668e88bae6eb21888acfccdc&redirect=http://localhost:63342/D_helper/index.html';
+    window.location.href = 'https://auth.dimigo.net/oauth?client=668e88bae6eb21888acfccdc&redirect=https://mujun0820.github.io/D_helper/';
 });
